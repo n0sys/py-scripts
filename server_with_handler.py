9765cc07d4d6011801c3caf260c5892dcb5b6c4c
Hanna Nassar <hannanassar1999@gmail.com>
2023-03-05 14:34:35 -0500

    Create server_with_handler.py

---
+++ server_with_handler.py
@@ -0,0 +1,22 @@
+import http.server
+import socketserver
+
+PORT = 8000
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+Handler.extensions_map={
+        '.manifest': 'text/cache-manifest',
+        '.html': 'text/html',
+        '.png': 'image/png',
+        '.jpg': 'image/jpg',
+        '.svg': 'image/svg+xml',
+        '.css': 'text/css',
+        '.js':  'application/x-javascript',
+        '': 'application/octet-stream', # Default
+    }
+
+httpd = socketserver.TCPServer(("", PORT), Handler)
+
+print("serving at port", PORT)
+httpd.serve_forever()
